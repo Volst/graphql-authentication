6235f9c19e483c3d026da4cb2267aae230111899
Kees Kluskens <kees@webduck.nl>
2018-06-17 16:05:30 +0200

    make adding an adapter required

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { mutations as authMutations } from './mutations';
 export { queries as authQueries } from './queries';
 export { getUser, getUserId, forwardTo, isAuthResolver } from './utils';
 export { graphqlUserConfig } from './Config';
+export { GraphqlUserPrismaAdapter } from './adapter/Prisma';

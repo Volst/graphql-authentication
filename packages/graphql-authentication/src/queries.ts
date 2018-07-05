fb47c80174c00b163495e3d6ad319d1441c195a9
Kees Kluskens <kees@webduck.nl>
2018-07-05 14:03:03 +0200

    add weird workaround for weird TS bug

--- packages/graphql-authentication/src/queries.ts
+++ packages/graphql-authentication/src/queries.ts
@@ -1,4 +1,6 @@
 import { getUserId, Context } from './utils';
+// Without this manual User interface import, TypeScript will create an incorrect queries.d.ts declaration file, WTF?
+import { User } from './Adapter';
 
 export const queries = {
   currentUser(parent: any, args: any, ctx: Context, info: any) {
